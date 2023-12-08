04cc2a4365bf4c19aa0728013f1a090c95cd7c8f
gaelinator <gaeliizii@gmail.com>
2023-12-08 18:25:10 +0000

    setting upthe modeule

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      MONGO_URI: Joi.string().required(),
+      }),
+    })
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
