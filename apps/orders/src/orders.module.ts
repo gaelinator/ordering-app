31fbe0b79925129596036fede9765d816e0d2750
gaelinator <gaeliizii@gmail.com>
2023-12-09 00:20:52 +0000

    dockerisation baby

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -3,15 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
 import { DatabaseModule } from '@app/common';
 import { Order, OrderSchema } from './schemas/order.schema';
+import { OrdersRepository } from './order.repository';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     validationSchema: Joi.object({
-      MONGO_URI: Joi.string().required(),
+      MONGODB_URI: Joi.string().required(),
       }),
       envFilePath: './apps/orders/.env',
     }),
@@ -19,6 +20,6 @@ import { Order, OrderSchema } from './schemas/order.schema';
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema}]),
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
